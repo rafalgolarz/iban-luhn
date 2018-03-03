c4853b3feff47a085ceaee2a58b01d2fd98a152a
Rafal Golarz <rafalg@watt3.com>
2018-03-03 13:46:31 +0100

    extending tests with goroutines and support for 1.10

--- accounts/accounts_test.go
+++ accounts/accounts_test.go
@@ -1,8 +1,18 @@
 package accounts
 
-import "testing"
+import (
+	"sync"
+	"testing"
+)
+
+/*	-count n
+Run each test and benchmark n times (default 1).
+If -cpu is set, run n times for each GOMAXPROCS value.
+*/
 
 //go test -v
+//go test -v -count 20 -benchmem -bench=.
+//go test -v -count 20 -cpu 1,8 -benchmem -bench=.
 func TestIBAN(t *testing.T) {
 
 	var tests = []struct {
@@ -32,4 +42,20 @@ func BenchmarkIBAN(b *testing.B) {
 	for i := 0; i < b.N; i++ {
 		IsIBAN("MT84 MALT 0110 0001 2345 MTLC AST0 01S")
 	}
+}
+
+//cpu value depends on your processor architecture
+//go test -count 3 -cpu 1,2,4,6,8 -benchmem -bench=.
+//The default value of cpu is the current value of GOMAXPROCS
+var wg sync.WaitGroup
+
+func BenchmarkConcIBAN(b *testing.B) {
+	wg.Add(b.N)
+	for i := 0; i < b.N; i++ {
+		go func() {
+			defer wg.Done()
+			IsIBAN("MT84 MALT 0110 0001 2345 MTLC AST0 01S")
+		}()
+	}
+	wg.Wait()
 }
--- cards/cards_test.go
+++ cards/cards_test.go
@@ -1,6 +1,9 @@
 package cards
 
-import "testing"
+import (
+	"sync"
+	"testing"
+)
 
 //go test -v
 func TestLuhn(t *testing.T) {
@@ -28,4 +31,20 @@ func BenchmarkLuhn(b *testing.B) {
 	for i := 0; i < b.N; i++ {
 		IsLuhn("378282246310005")
 	}
+}
+
+//cpu value depends on your processor architecture
+//go test -count 3 -cpu 1,2,4,6,8 -benchmem -bench=.
+//The default value of cpu is the current value of GOMAXPROCS
+var wg sync.WaitGroup
+
+func BenchmarkConcLuhn(b *testing.B) {
+	wg.Add(b.N)
+	for i := 0; i < b.N; i++ {
+		go func() {
+			defer wg.Done()
+			IsLuhn("378282246310005")
+		}()
+	}
+	wg.Wait()
 }
